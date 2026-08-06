427a637d8afd60182cdec4f321f56a88b0d905c3
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-08-06 21:51:35 +0300

    Описана структура списка файлов на диске с помощью JSON Schema и создан тест на проверку получения списка файлов

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,15 +1,18 @@
 from pathlib import Path
 
 import allure
+import jsonschema
 from pydantic import BaseModel, ValidationError
 
 
+@allure.step("Получение пути ресурса в корзине")
 def get_path_from_trash(response_data: dict, resource_title: str) -> str:
     """Получение пути ресурса в корзине"""
     items = items = response_data["_embedded"]["items"]
     return next(item["path"] for item in items if resource_title in item["path"])
 
 
+@allure.step("Генерация текстового файла")
 def generate_txt_file(text: str):
     """Генерация текстового файла"""
     file_path = Path("data.txt")
@@ -24,4 +27,13 @@ def validate_response(model: type[BaseModel], response):
     try:
         return model.model_validate(response.json())
     except ValidationError as e:
+        raise AssertionError(f"Ошибка валидации: {e}")
+
+
+@allure.step("Проверка JSON схемы")
+def validate_json_schema(json_data: dict, schema: dict):
+    """Проверка JSON схемы"""
+    try:
+        jsonschema.validate(json_data, schema)
+    except jsonschema.ValidationError as e:
         raise AssertionError(f"Ошибка валидации: {e}")
